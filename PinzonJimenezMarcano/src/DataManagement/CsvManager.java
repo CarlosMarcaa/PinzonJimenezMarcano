dc7268a15cccb02254076e78c876f2dd69585be3
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2023-06-21 21:49:47 -0400

    Transforms the guest's credentials to integers

--- PinzonJimenezMarcano/src/DataManagement/CsvManager.java
+++ PinzonJimenezMarcano/src/DataManagement/CsvManager.java
@@ -4,26 +4,31 @@
  */
 package DataManagement;
 
+import DataStructures.Guest;
 import DataStructures.HashTableClass;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
- * @author Miguel
+ * @author Miguel Jiménez
  */
 public class CsvManager {
-    
+
     public void ReadText(String path) {
-        HashTableClass hashTable = new HashTableClass(300);
-        
+        HashTableClass hashTable = new HashTableClass(301);
+
         File cvsFile = new File(path);
         BufferedReader lector;
-        
+
         String line;
         String data_cvs = "";
-        
+
         try {
             lector = new BufferedReader(new FileReader(cvsFile));
             while ((line = lector.readLine()) != null) {
@@ -31,7 +36,7 @@ public class CsvManager {
                     data_cvs += line + "\n";
                 }
             }
-            
+
             if (!"".equals(data_cvs)) {
                 String[] data_split = data_cvs.split("\n");
                 for (int i = 0; i < data_split.length; i++) {
@@ -39,26 +44,39 @@ public class CsvManager {
                     if (path.equals("test/Booking_hotel_reservations.csv") && !data_split[i].equals("ci,primer_nombre,segundo_nombre,email,genero,tipo_hab,celular,llegada,salida")) {
                         System.out.println(data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5] + ", " + data[6] + ", " + data[7] + ", " + data[8]);
                     } else if (path.equals("test/Booking_hotel_status.csv") && !data_split[i].equals("num_hab,primer_nombre,apellido,email,genero,celular,llegada")) {
-                        System.out.println(data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5] + ", " + data[6]);
+//                        System.out.println(data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5] + ", " + data[6]);
                         if (!data[0].equals("")) {
-                            hashTable.put(data[1], data[2], Integer.parseInt(data[0]));
-                        }                        
+                            Guest guest = new Guest(data[1], data[2]);
+                            hashTable.put(guest, Integer.parseInt(data[0]));
+                        }
                     } else if (path.equals("test/Booking_hotel_rooms.csv") && !data_split[i].equals("num_hab,tipo_hab,piso")) {
                         System.out.println(data[0] + ", " + data[1] + ", " + data[2]);
                     } else if (path.equals("test/Booking_hotel_historic.csv") && !data_split[i].equals("ci,primer_nombre,apellido,email,genero,llegada,num_hab")) {
-                        System.out.println(data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5] + ", " + data[6]);
+//                        System.out.println(data[0] + ", " + data[1] + ", " + data[2] + ", " + data[3] + ", " + data[4] + ", " + data[5] + ", " + data[6]);
+                        System.out.println(idToInt(data[0]));
                     }
                 }
 //                hashTable.printHashTable();
-            } 
-            
+            }
+
             lector.close();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-    
+
+    public int idToInt(String idString) {
+        String[] temp = idString.split("\\.");
+        if (temp.length == 2) {
+            idString = temp[0] + temp[1];
+        } else if (temp.length == 3) {
+            idString = temp[0] + temp[1] + temp[2];
+        }
+
+        int intId = Integer.parseInt(idString);
+        return intId;
+    }
+
     public void WriteText(String path) {
-        
     }
 }
