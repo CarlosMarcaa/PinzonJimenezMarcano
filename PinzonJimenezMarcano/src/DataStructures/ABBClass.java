e531eccbebdca98f61d9e9f95276dff5012bdde0
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2023-06-26 09:17:32 -0400

    ✨ Shows the history of a room

--- PinzonJimenezMarcano/src/DataStructures/ABBClass.java
+++ PinzonJimenezMarcano/src/DataStructures/ABBClass.java
@@ -2,16 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package DataStructures;
 
 import Objects.Guest;
 
 /**
  *
- * @author Carlol
+ * @author Carlos Marcano
+ * @author Miguel Jiménez
  */
 public class ABBClass {
+
     private ABBNode root;
 
     public ABBClass() {
@@ -25,149 +26,157 @@ public class ABBClass {
     public void setRoot(ABBNode root) {
         this.root = root;
     }
-    
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return root == null;
     }
-    
-    public void insert(Guest guest, ABBNode root){
+
+    public void insert(Guest guest, ABBNode root) {
         ABBNode nodo = new ABBNode(guest);
-        if (isEmpty()){
+        if (isEmpty()) {
             setRoot(nodo);
-        }else{
-                if (nodo.getElement() < root.getElement()){
-                    if (root.getLeftSon() == null){
-                        root.setLeftSon(nodo);
-                    }else{
-                        insert(guest, root.getLeftSon());
-                    }
-                }else{
-
-                    if (root.getRightSon() == null){
-                        root.setRightSon(nodo);
-                    }else{
-                        insert(guest, root.getRightSon());
-                    }            
+        } else {
+            if (nodo.getElement() < root.getElement()) {
+                if (root.getLeftSon() == null) {
+                    root.setLeftSon(nodo);
+                } else {
+                    insert(guest, root.getLeftSon());
                 }
-        }
-        
-        
-    }
-    
-    public void delete(int element, ABBNode previousNode, ABBNode root){
+            } else {
 
-        if (isEmpty()){
+                if (root.getRightSon() == null) {
+                    root.setRightSon(nodo);
+                } else {
+                    insert(guest, root.getRightSon());
+                }
+            }
+        }
+
+    }
+
+    public void delete(int element, ABBNode previousNode, ABBNode root) {
+
+        if (isEmpty()) {
             System.out.println("The tree is empty");
-        }else{
-            if (element != root.getElement()){
-                if (element < root.getElement()){
+        } else {
+            if (element != root.getElement()) {
+                if (element < root.getElement()) {
                     delete(element, root, root.getLeftSon());
-                }else{
+                } else {
                     delete(element, root, root.getRightSon());
                 }
-            }else{
+            } else {
                 //The root is leaf
-                if (root.isLeaf()){
-                if (previousNode == null){
-                    root = null;
-                }else{
-                    if (element > previousNode.getElement()){
-                        previousNode.setRightSon(null);
-                    }else{
-                        previousNode.setLeftSon(null);
+                if (root.isLeaf()) {
+                    if (previousNode == null) {
+                        root = null;
+                    } else {
+                        if (element > previousNode.getElement()) {
+                            previousNode.setRightSon(null);
+                        } else {
+                            previousNode.setLeftSon(null);
+                        }
                     }
                 }
-                }
-                
+
                 //The root only has right son
-                if (root.getLeftSon() == null && root.getRightSon() != null){
-                    if (previousNode == null){
+                if (root.getLeftSon() == null && root.getRightSon() != null) {
+                    if (previousNode == null) {
                         setRoot(root.getRightSon());
-                    }else{
-                        if (element > previousNode.getElement()){
+                    } else {
+                        if (element > previousNode.getElement()) {
                             previousNode.setRightSon(root.getRightSon());
-                        }else{
+                        } else {
                             previousNode.setLeftSon(root.getRightSon());
                         }
                     }
                 }
-                
+
                 //The root only has left son
-                if (root.getRightSon()== null && root.getLeftSon() != null){
-                    if (previousNode == null){
+                if (root.getRightSon() == null && root.getLeftSon() != null) {
+                    if (previousNode == null) {
                         setRoot(root.getLeftSon());
-                    }else{
-                        if (element > previousNode.getElement()){
+                    } else {
+                        if (element > previousNode.getElement()) {
                             previousNode.setRightSon(root.getLeftSon());
-                        }else{
+                        } else {
                             previousNode.setLeftSon(root.getLeftSon());
                         }
                     }
                 }
-                
+
                 //The root has both sons
-                if (root.getRightSon() != null && root.getLeftSon() != null){
+                if (root.getRightSon() != null && root.getLeftSon() != null) {
                     ABBNode replacement = searchReplacementNode(root.getLeftSon());
-                    if (previousNode == null){
+                    if (previousNode == null) {
                         setRoot(replacement);
-                    }else{
-                        if (replacement.getElement() > previousNode.getElement()){
+                    } else {
+                        if (replacement.getElement() > previousNode.getElement()) {
                             previousNode.setRightSon(replacement);
-                        }else{
+                        } else {
                             previousNode.setLeftSon(replacement);
                         }
                     }
-                    if (root.getLeftSon() != replacement){
+                    if (root.getLeftSon() != replacement) {
                         replacement.setLeftSon(root.getLeftSon());
                         replacement.setRightSon(root.getRightSon());
-                    }else{
+                    } else {
                         replacement.setRightSon(root.getRightSon());
-                        }
                     }
-                   }
-                    
                 }
-                
+            }
+
+        }
+
     }
-        
-    
-       
-    public void preOrden(ABBNode nodo){
-        if (nodo != null){
-        System.out.print(" [" + nodo.getElement() + "]");
-        preOrden(nodo.getLeftSon());
-        preOrden(nodo.getRightSon());
+
+    public void preOrden(ABBNode nodo) {
+        if (nodo != null) {
+            System.out.print(" [" + nodo.getElement() + "]");
+            preOrden(nodo.getLeftSon());
+            preOrden(nodo.getRightSon());
         }
     }
-    
-    public void inOrden(ABBNode nodo){    
-        if (nodo != null){
-        preOrden(nodo.getLeftSon());
-        System.out.print(" [" + nodo.getElement() + "]");
-        preOrden(nodo.getRightSon());
+
+    public void inOrden(ABBNode nodo) {
+        if (nodo != null) {
+            preOrden(nodo.getLeftSon());
+            System.out.print(" [" + nodo.getElement() + "]");
+            preOrden(nodo.getRightSon());
         }
     }
-    
-    public void postOrden(ABBNode nodo){   
-        if (nodo != null){
-        preOrden(nodo.getLeftSon());        
-        preOrden(nodo.getRightSon());
-        System.out.print(" [" + nodo.getElement() + "]");
+
+    public void postOrden(ABBNode nodo) {
+        if (nodo != null) {
+            preOrden(nodo.getLeftSon());
+            preOrden(nodo.getRightSon());
+            System.out.print(" [" + nodo.getElement() + "]");
         }
     }
-        
-    public void searchId(ABBNode root, int id){
-        if (root != null){
-            if (id == root.getElement()){
-                root.getGuest().printGuest();}
-        searchId(root.getLeftSon(), id);
-        searchId(root.getRightSon(), id);
+
+    public void searchId(ABBNode root, int id) {
+        if (root != null) {
+            if (id == root.getElement()) {
+                root.getGuest().printGuest();
+            }
+            searchId(root.getLeftSon(), id);
+            searchId(root.getRightSon(), id);
         }
     }
-        
-    
-    public ABBNode searchReplacementNode(ABBNode selected){
-        while (selected.getRightSon() != null){
+
+    public void searchRoom(ABBNode root, int room) {
+        if (root != null) {
+            if (room == root.getGuest().getRoom()) {
+                root.getGuest().printGuest();
+                System.out.println("");
+            }
+            searchRoom(root.getLeftSon(), room);
+            searchRoom(root.getRightSon(), room);
+        }
+    }
+
+    public ABBNode searchReplacementNode(ABBNode selected) {
+        while (selected.getRightSon() != null) {
             selected = selected.getRightSon();
         }
         return selected;
